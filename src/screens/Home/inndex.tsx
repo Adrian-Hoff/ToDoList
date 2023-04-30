f3d3b542e9d218544a3ecac9061248e128dcd45a
hykepashi <adrian.hoff2005@gmail.com>
2023-04-29 21:07:55 -0300

    feat: home task name and dinamical date

---
+++ src/screens/Home/inndex.tsx
@@ -0,0 +1,25 @@
+//expo
+import { StatusBar } from 'expo-status-bar';
+
+// react
+import { StyleSheet, Text, View } from 'react-native';
+
+//style
+import { styles } from './styles';
+
+export default function Home() {
+  const date = new Date()
+
+  const day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const month = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+  
+      
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <Text style={styles.taskName}>Task name</Text>
+      <Text style={styles.taskDate}>{day[date.getDay()] + ', ' + month[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()}</Text>
+    </View>
+  );
+}
